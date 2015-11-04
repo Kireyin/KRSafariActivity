b9dc7b2414dc94a6eb98accdb98f3d8321e6855e
Alexandre <abrispot@gmail.com>
2015-11-04 17:18:32 +0100

    v1.0.0

---
+++ KRSafariActivity/KRSafariActivity/KRSafariActivity.h
@@ -0,0 +1,19 @@
+//
+//  KRSafariActivity.h
+//  KRSafariActivity
+//
+//  Created by Alexandre Brispot on 04/11/15.
+//  Copyright © 2015 Kireyin (Alexandre Brispot). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KRSafariActivity.
+FOUNDATION_EXPORT double KRSafariActivityVersionNumber;
+
+//! Project version string for KRSafariActivity.
+FOUNDATION_EXPORT const unsigned char KRSafariActivityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KRSafariActivity/PublicHeader.h>
+
+
---
+++ KRSafariActivity/KRSafariActivity/KRSafariActivity.swift
@@ -0,0 +1,70 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Alexandre Brispot
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+
+public class KRSafariActivity: UIActivity {
+   private var url: NSURL?
+   public let KRSafariActivityType: String = "KRSafariActivity"
+   
+   public override func activityType() -> String? {
+      return KRSafariActivityType
+   }
+   
+   public override func activityTitle() -> String? {
+      return NSLocalizedString("activityTitle", tableName: "strings", bundle: NSBundle(forClass: KRSafariActivity.self), value: "", comment: "")
+   }
+   
+   public override func activityImage() -> UIImage? {
+      return UIImage(named: "KRSafariActivityIcon", inBundle: NSBundle(forClass: KRSafariActivity.self), compatibleWithTraitCollection: nil)
+   }
+   
+   public override func performActivity() {
+      guard let url = url else {
+         activityDidFinish(false)
+         return
+      }
+      activityDidFinish(UIApplication.sharedApplication().openURL(url))
+   }
+   
+   public override func canPerformWithActivityItems(activityItems: [AnyObject]) -> Bool {
+      for activityItem in activityItems {
+         if let url = activityItem as? NSURL {
+            if UIApplication.sharedApplication().canOpenURL(url) {
+               return true
+            }
+         }
+      }
+      return false
+   }
+   
+   public override func prepareWithActivityItems(activityItems: [AnyObject]) {
+      for activityItem in activityItems {
+         if let url = activityItem as? NSURL {
+            if UIApplication.sharedApplication().canOpenURL(url) {
+               self.url = url
+               return
+            }
+         }
+      }
+   }
+}
